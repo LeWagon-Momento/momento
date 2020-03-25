c066c34ab80b6fb405c279d924918f1374f073a2
Dexter Goh <bboifatt@gmail.com>
2020-03-26 02:50:37 +0800

    implemented users#show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'users/:id', to: 'users#show' # don't forget to commit this too
+  get 'users/:id', to: 'users#show'
   get 'users/:id/edit', to: 'users#edit' # took reference from lecture
   resources :trips do
     resources :posts

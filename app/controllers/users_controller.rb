56773f53fe657a492a0a63bd08dd5d95d628eb95
Dexter Goh <bboifatt@gmail.com>
2020-03-26 03:08:27 +0800

    implemented logic to user#show action by finding the params id of an instance of a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id]) # finding user through their id, used params through the console
+    @user = User.find(params[:id]) # why singular is because show is for individual users
   end
 
   def edit

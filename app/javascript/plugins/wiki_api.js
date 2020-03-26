d46d260e44426d5edbc86e813ccc44f8bc972d8c
kennchong <kennchong85@gmail.com>
2020-03-26 15:43:20 +0800

    fixing master

--- app/javascript/plugins/wiki_api.js
+++ app/javascript/plugins/wiki_api.js
@@ -1,30 +1,24 @@
 const wikiApi = () => {
-  const search = document.getElementById("wiki-button");
-  search.addEventListener('click', (event) => {
-    let searchValue = document.querySelector('#search')
-    console.log(searchValue.value);
-    const cityName = searchValue.value;
-    const url = `https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&titles=${cityName}&formatversion=2&exsentences=5&exlimit=1&explaintext=1`
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-    fetch(proxyUrl + url)
-    .then(response => response.json())
-    .then((data) => {
-      const retrievedInfo = data["query"]["pages"]["0"]["extract"]
-      const detail = document.querySelector('#wiki-info');
-      detail.placeholder = retrievedInfo;
+  if(search) {
+    search.addEventListener('click', (event) => {
+      let searchValue = document.querySelector('#search')
+      console.log(searchValue.value);
+      const cityName = searchValue.value;
+      const url = `https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&titles=${cityName}&formatversion=2&exsentences=5&exlimit=1&explaintext=1`;
+      const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+      fetch(proxyUrl + url)
+      .then(response => response.json())
+      .then((data) => {
+        const retrievedInfo = data["query"]["pages"]["0"]["extract"]
+        const detail = document.querySelector('#wiki-info');
+        detail.placeholder = retrievedInfo;
+      })
     });
-  })
+  }
 }
-​
-// const textCheck = () => {
-//   const search = document.getElementById("text-checkbox");
-//   search.addEventListener('click', (event) => {
-//     let text = document.querySelector('#wiki-info');
-//     text.value = text.placeholder;
-//   });
-// }
-​
+
 const textCheck = () => {
+  console.log('here');
   const search = document.querySelector(".text-checkbox");
   search.addEventListener('change', (event) => {
     if(search.checked) {
@@ -38,8 +32,5 @@ const textCheck = () => {
     }
   });
 }
-​
-export { wikiApi };
-export { textCheck };
 
-// This
+export { wikiApi, textCheck };

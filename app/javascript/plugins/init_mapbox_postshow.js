32196ceb378581195c8367867712d79434db2d20
kennchong <kennchong85@gmail.com>
2020-03-29 17:14:08 +0800

    did the postshow page layout and made the trip posts clickable to individual post show page but the range of it being clickabble is little buggy

--- app/javascript/plugins/init_mapbox_postshow.js
+++ app/javascript/plugins/init_mapbox_postshow.js
@@ -1,7 +1,14 @@
 import mapboxgl from 'mapbox-gl';
 
+
 const initPostShowMapbox = () => {
   const mapElement = document.getElementById('map1');
+
+  const fitMapToMarkers = (map, markers) => {
+    const bounds = new mapboxgl.LngLatBounds();
+    markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+    map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+  };
 
   if (mapElement) { // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
@@ -10,6 +17,15 @@ const initPostShowMapbox = () => {
       container: 'map1',
       style: 'mapbox://styles/mapbox/streets-v10'
     });
+
+    const markers = JSON.parse(mapElement.dataset.markers);
+    markers.forEach((marker) => {
+      new mapboxgl.Marker()
+        .setLngLat([ marker.lng, marker.lat ])
+        .addTo(map);
+    });
+
+    fitMapToMarkers(map, markers);
   }
 };
 

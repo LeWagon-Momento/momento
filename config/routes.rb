2a0d3a78f02806a1d948e288dec9234ea7fe4bc2
Dexter Goh <bboifatt@gmail.com>
2020-03-30 19:31:35 +0800

    routed admin controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   resources :trips do
     get '/review', to: 'trips#review', as: :trip
     resources :posts
+
+    namespace :admin do
+      resources :trips, only: [:index]
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
